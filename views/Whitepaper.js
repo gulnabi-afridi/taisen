a726bfd593da8c4297571be4ffc0fc6e4d287c8c
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-07-08 21:43:10 +0500

    our intro section has been added in the whitepaper

--- views/Whitepaper.js
+++ views/Whitepaper.js
@@ -1,15 +1,15 @@
 import Hero from "../components/Whitepaper/Hero";
 import OurTeam from "../components/Whitepaper/OurTeam";
-// import OutCommunity from "../components/Whitepaper/OutCommunity";
-// import OurIntro from "../components/Whitepaper/OurIntro";
+import OutCommunity from "../components/Whitepaper/OutCommunity";
+import OurIntro from "../components/Whitepaper/OurIntro";
 
 function Whitepaper() {
   return (
     <>
       <Hero />
       <OurTeam />
-      {/* <OutCommunity /> */}
-      {/* <OurIntro /> */}
+      <OutCommunity />
+      <OurIntro />
     </>
   );
 }
